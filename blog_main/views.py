8abe3772ab4b579e7f63c82cd4a8d8009d7648f5
sukeshreddy-m <sukeshreddymadireddy@gmail.com>
2026-03-07 18:50:46 +0530

    posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+    featured_blogs = Blog.objects.filter(is_featured = True)
+    posts = Blog.objects.filter(is_featured = False, status = 'Published')
+    context = {
+        'featured_blogs':featured_blogs,
+        'posts':posts,
+    }
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Blog, Category
 
-# Create your views here.
+def posts_by_category(request, category_id):
+    blogs_by_categories = Blog.objects.filter(category_id=category_id)
+    try:
+        category = Category. objects.get(pk=category_id)
+    except:
+        return redirect('home')
+    context = {
+        'blogs_by_categories' : blogs_by_categories,
+        'category' : category,
+    }
+    return render(request, 'posts_by_category.html', context)
