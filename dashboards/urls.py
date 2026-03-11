a706cb85e80e1dfb6a8c74385e60ad7f57532512
sukeshreddy-m <sukeshreddymadireddy@gmail.com>
2026-03-11 07:51:57 +0530

    posts CRUD features

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name = 'dashboard'),
-    path('categories', views.categories, name='categories'),
+    path('categories/', views.categories, name='categories'),
     path('categories/add', views.add_category, name='add_category'),
     path('categories/edit/<int:pk>', views.edit_category, name='edit_category'),
     path('categories/delete/<int:pk>', views.delete, name='delete'),
+    path('posts/', views.post, name='posts'),
+    path('posts/add_post/', views.add_post, name='add_post'),
+    path('posts/edit_post/<int:pk>', views.edit_post, name = 'edit_post'),
+    path('posts/delete/<int:pk>', views.delete_post, name='delete_post'),
 ]
