8021084e6135a021cd10644186ff0bdb0c5b0502
sukeshreddy-m <sukeshreddymadireddy@gmail.com>
2026-03-11 11:12:27 +0530

    manage user functionality for managers

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from blogs.models import Category, Blog
-from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm, BlogPostForm
+from django.contrib.auth.decorators import login_required, permission_required
+from .forms import CategoryForm, BlogPostForm, AddUserForm, EditUserForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 @login_required(login_url='login')
 def dashboard(request):
@@ -95,3 +96,44 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+def users(request):
+    users = User.objects.all()
+    context = {
+        'users':users,
+    }
+    return render(request, 'dashboards/users.html', context)
+
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = AddUserForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'dashboards/add_user.html', context)
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance = user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    form = EditUserForm(instance=user)
+    context = {
+        'user' : user,
+        'form' : form,
+    }
+    return render(request, 'dashboards/edit_user.html', context)
+
+@permission_required('auth.delete_user', raise_exception=True)
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
